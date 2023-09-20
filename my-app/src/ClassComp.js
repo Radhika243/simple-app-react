42a19efc9fa07a6511ace8f16dcb8578ff3d720b
Radhika <radhikakattics17@gmail.com>
2023-09-20 10:31:34 +0530

    functional and class component examples

---
+++ my-app/src/ClassComp.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+class ClassComp extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Hi Class Component</h1>
+                <h4>Shri Ram Jai Ram Jai Jai Ram </h4>
+            </div>
+        )
+    }
+}
+
+export default ClassComp;
